a11bf8824821e469f38277a8657c3a687fa51e3f
matt <mthw.wm.robinson@gmail.com>
2020-02-03 23:22:56 -0500

    no more convoluted versioning

--- heartcrm/__init__.py
+++ heartcrm/__init__.py
@@ -1,13 +1,4 @@
-import subprocess
-
 from heartcrm.conf import configure
 
 
-def get_version():
-    out = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0'])
-    version = out.decode('utf-8').strip()
-    return version
-
-
-__version__ = get_version()
 __all__ = [configure]
