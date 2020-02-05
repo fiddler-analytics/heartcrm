5c2a5fe1e4285970d969d9badf404d6c0c9386da
matt <mthw.wm.robinson@gmail.com>
2020-02-04 22:49:25 -0500

    overwriting the curret dir in config test

--- tests/test_configure.py
+++ tests/test_configure.py
@@ -3,6 +3,7 @@ import heartcrm.conf as conf
 
 def test_conf_writes_to_heartrc(monkeypatch, tmpdir):
     monkeypatch.setattr(conf, 'PROJECT_DIR', tmpdir.dirname)
+    monkeypatch.setattr(conf, 'CURRENT_DIR', tmpdir.dirname)
     desired_config = {'redirect_uri': 'https://parrots.com',
                       'client_id': 'super',
                       'client_secret': 'secret',
